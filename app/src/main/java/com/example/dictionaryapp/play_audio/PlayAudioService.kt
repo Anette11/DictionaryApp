e8d0217db37645b36cc32b57af1dc81043cadf61
Anette11 <krivaltseva@gmail.com>
2022-10-13 23:43:30 +0400

    Move play audio to coroutine

--- app/src/main/java/com/example/dictionaryapp/play_audio/PlayAudioService.kt
+++ app/src/main/java/com/example/dictionaryapp/play_audio/PlayAudioService.kt
@@ -6,11 +6,17 @@ import android.media.AudioAttributes
 import android.media.MediaPlayer
 import android.os.Binder
 import android.os.IBinder
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 class PlayAudioService : Service() {
     private val binder = LocalBinder()
-    private lateinit var mediaPlayer: MediaPlayer
+    private var mediaPlayer: MediaPlayer? = null
     private var isPlayingAudio = false
+    private var playAudioJob: Job? = null
+    private val coroutineScope = CoroutineScope(Dispatchers.IO)
 
     override fun onBind(p0: Intent?): IBinder = binder
 
@@ -22,24 +28,42 @@ class PlayAudioService : Service() {
         audioUrl: String
     ) {
         if (isPlayingAudio) return
-        try {
-            isPlayingAudio = true
-            mediaPlayer = MediaPlayer()
-            mediaPlayer.setAudioAttributes(
-                AudioAttributes.Builder()
-                    .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
-                    .build()
-            )
-            mediaPlayer.setDataSource(audioUrl)
-            mediaPlayer.prepare()
-            mediaPlayer.start()
-            mediaPlayer.setOnCompletionListener {
+        playAudioJob?.cancel()
+        playAudioJob = coroutineScope.launch {
+            try {
+                isPlayingAudio = true
+                mediaPlayer = MediaPlayer()
+                mediaPlayer?.setAudioAttributes(
+                    AudioAttributes.Builder()
+                        .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
+                        .build()
+                )
+                mediaPlayer?.setDataSource(audioUrl)
+                mediaPlayer?.prepare()
+                mediaPlayer?.start()
+                mediaPlayer?.setOnCompletionListener {
+                    isPlayingAudio = false
+                }
+            } catch (e: Exception) {
                 isPlayingAudio = false
+                mediaPlayer?.reset()
+                e.printStackTrace()
             }
+        }
+    }
+
+    override fun onUnbind(intent: Intent?): Boolean {
+        try {
+            playAudioJob?.cancel()
+            mediaPlayer?.stop()
+            mediaPlayer?.reset()
+            mediaPlayer = null
+            isPlayingAudio = false
         } catch (e: Exception) {
             isPlayingAudio = false
-            mediaPlayer.reset()
+            mediaPlayer?.reset()
             e.printStackTrace()
         }
+        return super.onUnbind(intent)
     }
 }
